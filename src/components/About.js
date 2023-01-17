d6f35387e2a2f4235736796f9391d5c887289f8c
BalajeeRJS <rjsb0804@gmail.com>
2023-01-17 17:27:19 +0530

    My First commit

---
+++ src/components/About.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+export default function About() {
+
+  const downloadTxtFile = () => {
+    fetch('https://github.com/BalajeeRJS/my_portfolio/blob/main/RJS%20Balajee.pdf', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/pdf',
+      },
+    })
+    .then((response) => response.blob())
+    .then((blob) => {
+      // Create blob link to download
+      const url = window.URL.createObjectURL(
+        new Blob([blob]),
+      );
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute(
+        'download',
+        `FileName.pdf`,
+      );
+  
+      // Append to html link element page
+      document.body.appendChild(link);
+  
+      // Start download
+      link.click();
+  
+      // Clean up and remove the link
+      link.parentNode.removeChild(link);
+    });
+}
+  return (
+    <section id="about" >
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Balajee.
+            <br className="hidden lg:inline-block" />I build amazing
+            Websites.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            
+          </p>
+          <div className="flex justify-center">
+      
+            <div className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+     <button id="downloadBtn" onClick={downloadTxtFile} value="download">My Resume</button>
+</div>
+            <a
+              href="#contact"
+              className="ml-4 inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+             Contact Me
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded-full"
+            alt="hero"
+            src="./balajee.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const projects = [
+    {
+      title: "Unmanned Ground Vehicle",
+      subtitle: "Internet Of Things",
+      description:
+        "This is a semi-automated Unmanned Ground Vechile which has been selected for ZONAL LEVEL COMPETITION 2018(SOUTH ZONE) conducted by DRDO @ 60th",
+      image: "./DRDO.jpeg",
+      link: "",
+    },
+    {
+      title: "E-Commerce website",
+      subtitle: "PHP and SQL",
+      description:
+        "An E-Commerce website to sell Groceries directly from Farmers.This project was selected as top 3 for SMART INDIA HACKATHON 2019",
+      image: "./SMART INDIA HACKATHON.jpeg",
+      link: "",
+    },
+
+  ];
+
+  
+export const skills = [
+    "Java",
+    "Spring Boot",
+    "JavaScript",
+    "React Js",
+    "Node Js",
+    "Unix Scripting",
+    "Mongo DB",
+    "SQL"
+  ];
+  
