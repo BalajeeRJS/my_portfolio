1d196570e9f612424a075bc21abf56e0a6dd3285
BalajeeRJS <rjsb0804@gmail.com>
2023-01-21 12:39:11 +0530

    Changed anchor tag in project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,7 +15,7 @@ export default function Projects() {
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-            <a
+            <p
               href={project.link}
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
@@ -35,7 +35,7 @@ export default function Projects() {
                   <p className="leading-relaxed">{project.description}</p>
                 </div>
               </div>
-            </a>
+            </p>
           ))}
         </div>
       </div>
