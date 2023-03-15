c58cb01e64eb51a790365d5e836b298284ea521c
BalajeeRJS <rjsb0804@gmail.com>
2023-03-15 23:32:01 +0530

    Updated data.js

--- src/data.js
+++ src/data.js
@@ -1,12 +1,5 @@
 export const projects = [
-  {
-    title: "Phonepe Pulse Data Analysis",
-    subtitle: "Python, Streamlit, AWS MySQL, Pandas, Plotly",
-    description:
-      "This is Streamlit web app for analysing and visualizing Phonepe Pulse Data (2018-2022).The unstructured pluse data are cloned and transformed to structure data. Then,the structure data are stored in AWS RDS Mysql database.",
-    image: "./Phonepepulse.jpeg",
-    link: "https://balajeerjs-phonepe-pulse-data-analysis-main-b2jniv.streamlit.app/",
-  },
+
   {
     title: "Twitter Scrapping",
     subtitle: "Python, Streamlit, Mongo DB, Pandas, Snscrape",
@@ -14,6 +7,14 @@ export const projects = [
       "This is Streamlit web app for Scraping Twitter.It scrapes the twitter data for the given hashtag/keyword in a specific date range using snscrape library.The retrieved tweets are uploaded in MongoDB and can be downloaded as CSV or JSON file.",
     image: "./twitterscrap.jpeg",
     link: "https://balajeerjs-twitter-scrap-main-8w2auu.streamlit.app/",
+  },
+  {
+    title: "Phonepe Pulse Data Analysis",
+    subtitle: "Python, Streamlit, AWS MySQL, Pandas, Plotly",
+    description:
+      "This is Streamlit web app for analysing and visualizing Phonepe Pulse Data (2018-2022).The unstructured pluse data are cloned and transformed to structure data. Then,the structure data are stored in AWS RDS Mysql database.",
+    image: "./Phonepepulse.jpeg",
+    link: "https://balajeerjs-phonepe-pulse-data-analysis-main-b2jniv.streamlit.app/",
   },
     {
       title: "Unmanned Ground Vehicle",
