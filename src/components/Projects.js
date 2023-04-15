66e8edf88eaf6d757e9198411c8b3559000a55ca
BalajeeRJS <rjsb0804@gmail.com>
2023-04-15 13:07:43 +0530

    updated project.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,7 +25,7 @@ export default function Projects() {
               <div className="flex relative" style={{height:"300px" }}>
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-contain object-center"
+                  className="absolute inset-0 w-full h-full object-fill object-center"
                  src={project.image}
                 />
                 <div className="px-8 py-10 relative w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
