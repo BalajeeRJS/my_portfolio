196af4b747c128d0c0ff58397c719dd6f7c4fd82
BalajeeRJS <rjsb0804@gmail.com>
2023-04-15 12:16:07 +0530

    Skills updated

--- src/data.js
+++ src/data.js
@@ -15,23 +15,31 @@ export const projects = [
       "This is Streamlit web app for analysing and visualizing Phonepe Pulse Data (2018-2022).The unstructured pluse data are cloned and transformed to structure data. Then,the structure data are stored in AWS RDS Mysql database.",
     image: "./Phonepepulse.jpeg",
     link: "https://balajeerjs-phonepe-pulse-data-analysis-main-b2jniv.streamlit.app/",
+  }, 
+  {
+    title: "Automatic data collection using AWS Lambda",
+    subtitle: "Python, AWS MySQL, AWS Lambda, AWS Cloudwatch, AWS SNS",
+    description:
+      "This project is designed using AWS Lambda and AWS Cloud watch for an automatic data collection and storage system with server availability monitoring. If AWS Lambda throws an error, error messages are sent to the Slack channel using AWS SNS.",
+    image: "./lambda.jpeg",
+    link: "https://github.com/BalajeeRJS/AWS_lambda",
   },
-    {
+  {
       title: "Unmanned Ground Vehicle",
       subtitle: "Internet Of Things",
       description:
         "This is a semi-automated Unmanned Ground Vechile which has been selected for ZONAL LEVEL COMPETITION 2018(SOUTH ZONE) conducted by DRDO @ 60th",
       image: "./DRDO.jpeg",
       link: "",
-    },
-    {
+  },
+  {
       title: "E-Commerce website",
       subtitle: "PHP and SQL",
       description:
         "An E-Commerce website to sell Groceries directly from Farmers.This project was selected as top 3 for SMART INDIA HACKATHON 2019",
       image: "./SMART INDIA HACKATHON.jpeg",
       link: "",
-    },
+  },
 
   ];
 
@@ -48,5 +56,5 @@ export const skills = [
     "Spring Boot",
     "JavaScript",
     "React Js",
-  ];
+     ];
   
